27ae196b73cbb4592d199de532096297224ac0c7
Iliasse <elabouyi.iliasse@hotmail.fr>
2022-11-25 14:54:57 +0100

    "media queries: mobile"

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 import { WeatherService } from "./service/weatherService.js";
-import { ViewBuilder } from "./UI/viewBuilder.js"
+import { ViewBuilder } from "./UI/viewBuilder.js";
 
-// Create parameters
-const insee = 30189;
-const weatherService = new WeatherService();
-let viewBuilder;
+// récupérer l'entrée utilisateur
 
-// Get data
-Promise.all([weatherService.getWeather(insee), weatherService.getEphemeride(insee), weatherService.getWeatherThreeDays(insee), weatherService.getSaintsOfTheDay()])
-.then(response => {
+// Creation des arguments / informations
+const ville = { insee: 30189, libelle: "Nimes" };
+const weatherService = new WeatherService(); // init le service
+let viewBuilder; // init le service
+
+// Regrouper toutes les informations rassemblés
+Promise.all([
+  weatherService.getWeather(ville.insee),
+  weatherService.getEphemeride(ville.insee),
+  weatherService.getThreeDaysWeather(ville.libelle),
+  weatherService.getSaintsOfTheDay(),
+]).then((response) => {
   console.log(response);
-  // then build the page
-  viewBuilder = new ViewBuilder(response[0], response[1], response[2], response[3], document);
+  // then construire la page
+  viewBuilder = new ViewBuilder(
+    response[0],
+    response[1],
+    response[2],
+    response[3],
+    document
+  );
   viewBuilder.buildEphemeride();
   viewBuilder.buildTemperatures();
   viewBuilder.buildTable();
-})
-
-
-
-// Build the page out of data
+});
+// Si non afficher un chargeur de page css
